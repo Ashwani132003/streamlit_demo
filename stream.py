05301a32c8732c2d362d9666f8f516a1f81563a3
STAR SIWACH <76273539+Ashwani132003@users.noreply.github.com>
2022-05-27 21:58:04 +0530

    Update stream.py

--- stream.py
+++ stream.py
@@ -3,18 +3,23 @@ import streamlit as st
 import time
 
 
+from streamlit_webrtc import webrtc_streamer
 st.title("Web app")
-run = st.checkbox("Run")
-frame = st.image([])
 
-cap = cv2.VideoCapture(0)
+webrtc_streamer(key="sample")
 
-while run:
-    ret, Frame = cap.read()
-    time.sleep(5)
-    st.write("Wait ends")
-    Frame = cv2.cvtColor(Frame, cv2.COLOR_BGR2RGB)
-    frame.image(Frame)
+
+# run = st.checkbox("Run")
+# frame = st.image([])
+
+# cap = cv2.VideoCapture(0)
+
+# while run:
+#     ret, Frame = cap.read()
+#     time.sleep(5)
+#     st.write("Wait ends")
+#     Frame = cv2.cvtColor(Frame, cv2.COLOR_BGR2RGB)
+#     frame.image(Frame)
     
-else:
-    st.write("Stopped")   
+# else:
+#     st.write("Stopped")   
